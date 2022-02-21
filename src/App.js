1da10a07a5ed98a12653f25aeca58221d0084dda
Hojya <hohojya@gmail.com>
2022-02-21 17:45:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import image from './shopping.jpg'
+
 import './App.css';
 
 function App() {
   return (
     <div >
       <div>
-        <img src={image} width = '300px'/>
+      <h1>
+    СПИСОК ПОКУПОК
+  </h1>
       </div>
       <div>
         <h1>
     СПИСОК ПОКУПОК
   </h1>
       </div>
-  
+      git add .
     </div>
   );
 }
